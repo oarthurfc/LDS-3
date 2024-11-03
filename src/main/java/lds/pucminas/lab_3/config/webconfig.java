bcad9c4d768ba1f17c1b988e6ce9ce9755511e1a
oarthurfc <arthuryouface@gmail.com>
2024-11-03 20:18:47 -0300

    Permitindo requisição de novas rotas no CORS

--- src/main/java/lds/pucminas/lab_3/config/webconfig.java
+++ src/main/java/lds/pucminas/lab_3/config/webconfig.java
@@ -1,18 +1,17 @@
 package lds.pucminas.lab_3.config;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class webconfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // Habilitar CORS para os endpoints que começam com /api/
-                .allowedOrigins("http://127.0.0.1:5500") // Permitir o frontend de 127.0.0.1:5500
+                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:8081") // Inclui localhost:8081 como origem permitida
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos HTTP permitidos
                 .allowedHeaders("*"); // Permitir todos os cabeçalhos
     }
 }
-
